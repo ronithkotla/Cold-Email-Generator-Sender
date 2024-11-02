5078c6d1449560807020c3fd46f7cc60594b2612
Kotla Ronith <165662580+ronithkotla@users.noreply.github.com>
2024-11-02 13:25:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import streamlit as st
-__import__('pysqlite3') 
-import sys 
-sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
 from chains import Chain
 from portfolio import Portfolio
 from utils import clean_text
