b08084d60e27d8972a98ca8b74d74a98132449f9
Kotla Ronith <165662580+ronithkotla@users.noreply.github.com>
2024-12-28 11:41:17 +0530

    Update chains.py

--- chains.py
+++ chains.py
@@ -32,7 +32,7 @@ class Chain:
             raise OutputParserException("Context too big. Unable to parse jobs.")
         return res if isinstance(res, list) else [res]
 
-    def write_mail(self, job, links):
+    def write_mail(self, job, links,skills):
         prompt_email = PromptTemplate.from_template(
             """
             ### JOB DESCRIPTION:
@@ -45,15 +45,15 @@ class Chain:
             and we provide better placements to eligible students.
             Your job is to write a cold email to the client regarding the job mentioned above describing the capability of Bharat Institute of Engineering and Technology students who are eligible, 
             in fulfilling their needs.
-            Also add the most relevant ones from the following Linkedin links of students from my chromadb database along with their names to showcase Students details : {link_list} 
+            Also add the most relevant ones from the following Linkedin links of students from my chromadb database along with their names and skills {skills}to showcase Students details : {link_list} 
             Remember you are S.M.Raw,Placements Incharge at Bharat Institute of Engineering and Technology . 
-            Do not provide a preamble.
+            Do not provide a preamble.Donot give more line spaces.
             ### EMAIL (NO PREAMBLE):
 
             """
         )
         chain_email = prompt_email | self.llm
-        res = chain_email.invoke({"job_description": str(job), "link_list": links})
+        res = chain_email.invoke({"job_description": str(job), "link_list": links,"skills":skills})
         return res.content
 
 if __name__ == "__main__":
